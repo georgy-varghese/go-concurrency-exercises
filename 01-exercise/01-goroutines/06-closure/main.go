7b4526de44e0fc5af7b3a0c06be2bdfb9b4d9fb9
Georgy Varghese <30463346+georgy-varghese@users.noreply.github.com>
2025-04-11 16:02:43 +1200

    Solved '06-closure' exercise

--- 01-exercise/01-goroutines/06-closure/main.go
+++ 01-exercise/01-goroutines/06-closure/main.go
@@ -13,10 +13,16 @@ func main() {
 
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go func() {
+		// Passing i as variable to goroutine
+		// In tutorial, output without this change is 4 4 4
+		// It might seem there is no difference in output with/without this change
+		// However, not passing i as argument to goroutine, program is error prone and unreliable
+		// On passing the argument, a copy of i is created for each goroutine
+		// so value is not affected by loops subsequent iteration
+		go func(i int) {
 			defer wg.Done()
 			fmt.Println(i)
-		}()
+		}(i)
 	}
 	wg.Wait()
 }
